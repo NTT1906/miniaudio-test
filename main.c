0a87cef03c66e358899c1cd087db19614a35456f
Arie <nttispay@gmail.com>
2024-03-26 01:11:31 +0700

    Backup: yay

--- main.c
+++ main.c
@@ -11,9 +11,17 @@ void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uin
 	ma_decoder_read_pcm_frames(&decoder, pOutput, frameCount, NULL);
 
 	float* out = (float*)pOutput;
-	for (int i = 0; i < frameCount * pDevice->playback.channels; ++i) {
-		*out *= globalVolume;
-		out++;
+//	for (int i = 0; i < frameCount * pDevice->playback.channels; i += 2) {
+////		*out *= globalVolume;
+//		out[i] *= globalVolume;
+//		out[i+1] *= 1.0f - globalVolume;
+//		out += 2;
+//	}
+	for (int i = 0; i < frameCount; ++i) {
+		// Adjust volume for left channel
+		out[2*i] *= globalVolume;
+		// Adjust volume for right channel
+		out[2*i + 1] *= 1.0f - globalVolume;
 	}
 }
 
@@ -29,7 +37,7 @@ void setVolume(float volume) {
 
 int main() {
 	// Initialize miniaudio decoder
-	ma_result result = ma_decoder_init_file("Mili - String Theocracy.mp3", NULL, &decoder);
+	ma_result result = ma_decoder_init_file("NECRY TALKIE - bloom ｜UPBEAT - Piano Cover.mp3", NULL, &decoder);
 	if (result != MA_SUCCESS) {
 		printf("Failed to initialize decoder\n");
 		return -1;
@@ -78,6 +86,7 @@ int main() {
 		}
 		printf("Current volume: %f\n", globalVolume + i * 0.01f);
 		setVolume(globalVolume + i*0.01f);
+		ma_sleep(10);
 	}
 	// Cleanup
 	ma_device_uninit(&device);
